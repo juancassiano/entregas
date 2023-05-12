c2f191e8dd4c3a3a5e6036f0bc0a4d79bb51c1ad
juancassiano <juancassiano@hotmail.com>
2023-05-12 16:43:24 -0300

    Implements routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,19 @@
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+import { AuthenticateClientController } from './modules/account/authenticateClientUseCase/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { Router } from "express";
+import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliveryController = new AuthenticateDeliverymanController();
+
 
 routes.post("/clients/", createClientController.handle);
-
+routes.post("/deliveryman", createDeliverymanController.handle);
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post("/deliveryman/authenticate", authenticateDeliveryController.handle);
 export {routes};
